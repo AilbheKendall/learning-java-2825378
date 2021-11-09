1c2cb6b60f8ee371be481947be828ff783a6ed54
AilbheKendall <88047324+AilbheKendall@users.noreply.github.com>
2021-11-09 10:05:14 +0000

    commited on 2_05

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         double studentGPA = 3.45;
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
+
+        System.out.println(studentFirstName + " " + 
+                        studentLastName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+        input.close();
+
+        System.out.println(studentFirstName + " " + 
+                    studentLastName + " now has a GPA of " + studentGPA);
+                    
     }
 }
